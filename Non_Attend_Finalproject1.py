8c536c01d59ed5ae80d9176029bf7679b0129726
Mina Sharafi <mina.sharafi20@gmail.com>
2025-01-31 21:44:17 +0330

    Update Non_Attend_Finalproject1.py

--- Non_Attend_Finalproject1.py
+++ Non_Attend_Finalproject1.py
@@ -974,7 +974,7 @@ class AdminPanel:
         devices=self.devices_ingroups(group_name)
         for device in devices:
             device.turn_on()
-            
+        print(f'Turnini on all device in {group_name}.')    
             
             
     def turn_off_all_in_group(self,group_name):
@@ -982,6 +982,7 @@ class AdminPanel:
         devices=self.devices_ingroups(group_name)
         for device in devices:
             device.turn_off()
+        print(f'Turnini off all device in {group_name}.')
         
         ''' hameye cheragh haye yek goroh ra khamosh konad'''
         
