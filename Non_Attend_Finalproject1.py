a00059b08a0c1a2348f660c178e37a7bf063fd9f
Mina Sharafi <mina.sharafi20@gmail.com>
2025-02-01 19:16:41 +0330

    Update Non_Attend_Finalproject1.py

--- Non_Attend_Finalproject1.py
+++ Non_Attend_Finalproject1.py
@@ -1039,21 +1039,21 @@ class AdminPanel:
 
 
 
-a1=AdminPanel()
+if __name__ == '__main__':
+    a1=AdminPanel()
 
-a1.create_group('living_room')
-a1.create_multiple_devices('living_room','lamps',40)
+    a1.create_group('living_room')
+    a1.create_multiple_devices('living_room','lamps',40)
 
-a1.turn_on_all_in_group('living_room')
-a1.turn_off_all_in_group('living_room')
-#check koni bbini roshan 
+    a1.turn_on_all_in_group('living_room')
+    a1.turn_off_all_in_group('living_room')
+    #check koni bbini roshan
 
-mygroups=a1.groups['living_room']
+    mygroups=a1.groups['living_room']
 
-mygroups[1].name #lamps2'
+    #mygroups[1].name #lamps2'
 
-mygroups[1].turn_on()
-
+    #mygroups[1].turn_on()
         
 
 #besazi
